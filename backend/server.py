1d77d3f6092c1c806d592eb790e5a38af295e2ef
AndrewHarnor <asharnor@hotmail.com>
2026-07-13 17:50:05 +0100

    backend : Fix type - jumbled first line

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-arom fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from openai import OpenAI
