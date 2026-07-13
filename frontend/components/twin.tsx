2e18b6ec53fa878be0214deafe0a36ab69ebfb55
AndrewHarnor <asharnor@hotmail.com>
2026-07-13 10:47:05 +0100

    frontend -> twin.tsx onst response = await fetch(NEXT_PUBLIC_API_URL/chat

--- frontend/components/twin.tsx
+++ frontend/components/twin.tsx
@@ -42,8 +42,8 @@ export default function Twin() {
 
         try {
          // const response = await fetch('http://localhost:8000/chat', {
-            const response = await fetch('https://6pc8dpxa07.execute-api.eu-north-1.amazonaws.com/chat', {
-         // const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'}/chat`, {
+         // const response = await fetch('https://6pc8dpxa07.execute-api.eu-north-1.amazonaws.com/chat', {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'}/chat`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
